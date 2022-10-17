83fe7314cea239d2327b8ff22ee180bc40cf1cde
Edward Whitehead <edward@ultracoolbru.com>
2022-10-17 12:52:18 +0300

    Add project files.

---
+++ MonkeyFinder/MonkeyFinder/MauiProgram.cs
@@ -0,0 +1,22 @@
+﻿using MonkeyFinder.View;
+
+namespace MonkeyFinder;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+            .UseMauiCommunityToolkit()
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		builder.Services.AddSingleton<MainPage>();
+
+		return builder.Build();
+	}
+}
